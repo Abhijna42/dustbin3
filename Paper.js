48775f33aa1a990271e3b8c8cf780855513ff993
Abhijna42 <74900832+Abhijna42@users.noreply.github.com>
2020-12-16 14:08:39 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x,y,radius){
+        var options={
+            
+restitution:0.1,
+friction:1,
+density:1.3
+
+
+        }
+        this.body=Bodies.circle(x,y,radius,options);
+        this.radius = radius;
+        this.image = loadImage("paper.png");
+        World.add(world,this.body);
+    }
+    display()
+    {
+        var pos=this.body.position;
+        //var angle=this.body.angle;
+        push(); 
+        //translate(pos.x,pos.y);
+        fill("pink");
+        //rotate(angle);
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.radius,this.radius);
+        pop();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# dustbin3
+# p5.play-boilerplate
+Boiler plate for p5.play
